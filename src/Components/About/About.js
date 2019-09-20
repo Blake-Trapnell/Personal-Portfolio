05e4f61e2b6035e2ea27a560fa02bc62436cf798
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-09-19 21:04:37 -0600

    update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <div className="About_Outer">
             <div className="Family_Picture"></div>
             <h1>My name is Blake Trapnell, I come from a family of 9. <p>
-                 I'm a full stack web Developer. I graduated from DevMountain</p>
+                 I'm a full stack Web Developer. I graduated from DevMountain</p>
                  <p>in 2019. I enjoy making fun, interactive websites with functional</p> <p>
                      use. I'm an animal lover, I have a soft spot particularily for Hedgehogs</p>
                     <p>I enjoy getting to know people, and playing games. </p>
